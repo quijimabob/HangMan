4fa618898ce453a58c52ffe0f3015972c1652c56
quijimabob <kiss_the_rail@hotmail.com>
2023-09-20 11:31:16 +0100

    Another change to Hello World message

--- HangMan/Program.cs
+++ HangMan/Program.cs
@@ -6,7 +6,7 @@ namespace HangMan // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! I edited this from VS");
+            Console.WriteLine("Hello World! Mr Scott's not a cowering butterfly, but a beasty bee.");
         }
     }
 }
