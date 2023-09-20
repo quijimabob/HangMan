3bb2011e6a2020ed64dba51803ee10b788de4606
quijimabob <kiss_the_rail@hotmail.com>
2023-09-20 11:44:17 +0100

    One more change to Hello World, removing reference to Mr Scott

--- HangMan/Program.cs
+++ HangMan/Program.cs
@@ -6,7 +6,7 @@ namespace HangMan // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Mr Scott's not a cowering butterfly, but a beasty bee.");
+            Console.WriteLine("Hello World again!");
         }
     }
 }
